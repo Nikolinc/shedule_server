38a5b1cbc942698a8f02b17f2cf0470d8d7dda54
Nikolinc <tsigulskynikita@gmail.com>
2023-09-23 18:09:48 +0500

    add task service

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
+@ApiTags('Task')
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+  constructor(private taskServer: TaskService) {}
+
+  @ApiOperation({ summary: 'Create task' })
+  @Post('/create')
+  create(@Body() dto: CreateTaskDto) {
+    return this.taskServer.CreateTask(dto);
+  }
+
+  @ApiOperation({ summary: 'find all task' })
+  @Get('/all')
+  getAll() {
+    return this.taskServer.findAll();
+  }
+
+  @ApiOperation({ summary: 'find one task' })
+  @Get(':value')
+  findOne(@Param('value') value: string){
+    return this.taskServer.findOne(value);
+  }
+
+  @ApiOperation({ summary: 'set state task' })
+  @Post('/setState')
+  setState(@Body() props:{id:string,state:boolean}) {
+    return this.taskServer.setState(props.id,props.state);
+  }
+
+  @ApiOperation({ summary: 'delete task' })
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.taskServer.delite(id);
+  }
+  
+}
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Task } from './task.table';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
-export class TaskService {}
+export class TaskService {
+
+  constructor(
+    @InjectModel(Task) private taskRepository: typeof Task,
+  ){}
+
+  async CreateTask(dto:CreateTaskDto):Promise<Task>{
+    const task = this.taskRepository.create(dto);
+    return task
+  }
+  
+  async findOne(id:string):Promise<Task>{
+    const task = this.taskRepository.findOne({ where: { id } });
+    return task
+  }
+
+  async findAll():Promise<Task[]>{
+    const tasks = this.taskRepository.findAll();
+    return tasks
+  }
+
+  async setState(id:string, state:boolean):Promise<Task>{
+    const task = this.taskRepository.findOne({ where: { id } });
+    (await task).state = state;
+    return task
+  }
+
+  async delite(id:string):Promise<Task>{
+    const task = (await this.taskRepository.findOne({ where: { id } })).deletedAt;    
+    return task
+  }
+
+}
